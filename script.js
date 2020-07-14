6a8a3028cf8ac814eff3972e007b1d4f6b17255d
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-14 19:29:58 -0400

    added current time to console and changed button ids

---
+++ script.js
@@ -0,0 +1,7 @@
+// THIS IS CURRENT TIME FORMAT AND LOGS EXACT TIME (D,M,D,Y,T)
+let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentTime);
+
+$(document).ready(function () {
+  currentTime;
+});
