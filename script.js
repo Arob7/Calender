ac250cd805c8e58c65cda979c7b2d693a10b1af4
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-15 17:51:57 -0400

    added variables for input and button works in console.log

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 console.log(currentTime);
 
+// DISPLAYS CURRENT TIME ON PAGE FROM PARAGRAH ID
 function displayCurrentTime() {
   document.getElementById("currentDay").innerHTML = currentTime;
 }
 displayCurrentTime();
 
-// $(document).ready(function () {
-//   currentTime;
-// });
+//
+$(document).ready(function () {
+  $(".save-btn").on("click", function () {
+    // THIS WILL GRAB THE CURRENT HOUR
+    let time = $(this).parent().attr("id");
+    console.log(time);
+
+    // THIS WILL GRAB THE INPUT VALUE
+    let input = $(this).parent().siblings(".info").val();
+    console.log(input);
+
+    // STORES DATA FROM INPUT
+    localStorage.setItem(time, input);
+    console.log(time, input);
+
+    localStorage.getItem(".save-btn");
+  });
+});
